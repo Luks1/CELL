41b5c43216f51ec315da86463e6ecc99f0fb9c02
luxolo <luxolo@projectcodex.co>
2016-04-07 13:22:31 +0200

    cell work

---
+++ cell.js
@@ -0,0 +1,18 @@
+
+//my first string
+  var cost1 =     '“It was 67MB. Is that everything? I’m busy, you know.”'.match(/\d+/g);
+  console.log(cost1);
+
+
+//second string
+ var cost2 =  '“Damn, it was like 300MB. Dat Snaptalk is a data hog, yo.”'.match(/\d+/g);
+ console.log(cost2);
+
+//third array
+   var cost3 =  '“I think 283MB. That’s what the guy at the store said. I think it means MegaBites.”'.match(/\d+/g);
+   console.log(cost3);
+
+
+//fourth array
+ var cost4 =  '“Around 150MB. I have the receipts if you need more precise figures.”'.match(/\d+/g);
+ console.log(cost4);
